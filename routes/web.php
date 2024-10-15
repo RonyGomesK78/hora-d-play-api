ba60c66589ccd16a5c9f0e1822e118f659e0887e
Romilton Gomes <gromilton7@gmail.com>
2024-10-15 20:14:43 -0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $string = 'hello world';
+    return [
+        'name' => $string,
+        'details' => 'required'
+    ];
 });
